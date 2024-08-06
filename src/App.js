efe4cb6b9a5b297b34a65a03c49a9c6d5518af43
ArslanMani <arslanranglerz@gmail.com>
2024-08-06 23:54:16 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Invoice from './pages/Invoice/Invoice';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
-    <Invoice/>
+    <Router>
+      <Routes>
+      <Route path='/' element={<Invoice/>}/>   
+    </Routes>
+    </Router>
   );
 }
-
 export default App;
